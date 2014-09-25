723627ef1fcdc9a155838beadd8e84975896556c
Insung Yoon <insungyoon@outlook.com>
2014-09-25 12:27:56 +0900

    continue Example

---
+++ Class/src/class0925/ContinueExample1.java
@@ -0,0 +1,14 @@
+package class0925;
+
+public class ContinueExample1 {
+
+	public static void main(String[] args) {
+		for (int cnt = 0; cnt < 10; cnt++) {
+			if (cnt == 5)
+				continue;
+			System.out.println(cnt);
+		}
+		System.out.println("done!!");
+	}
+
+}
