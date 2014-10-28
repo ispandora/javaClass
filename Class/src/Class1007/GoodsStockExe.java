7c290c72582f66edc32932d6a990e2a6f3984589
insung <insungyoon@outllook.com>
2014-10-28 17:34:14 +0900

    Test push a windows computer

--- Class/src/Class1007/GoodsStockExe.java
+++ Class/src/Class1007/GoodsStockExe.java
@@ -2,7 +2,7 @@ package Class1007;
 
 public class GoodsStockExe {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws StockShortageException {
 		GoodsStock obj = new GoodsStock("A-1",5);
 		obj.addStock(5);
 		obj.subtractStock(11);
